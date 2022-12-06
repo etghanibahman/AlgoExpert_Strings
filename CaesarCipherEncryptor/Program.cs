555082523dcf2ae0c91ad1eff05b4edc39c49978
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2022-12-06 23:27:37 +0100

    Caesar_Cipher_Encryptor__Adding_Algo's Method

--- CaesarCipherEncryptor/Program.cs
+++ CaesarCipherEncryptor/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace CaesarCipherEncryptor
 {
@@ -8,40 +9,62 @@ namespace CaesarCipherEncryptor
     {
         static void Main(string[] args)
         {
+            //zab
             string sample = "xyz";
             int key = 2;
-            sample = "abc";
-            key = 57;
+
+            ////fgh
+            //sample = "abc";
+            //key = 57;
+
             Console.WriteLine($"The result is : {CaesarCypherEncryptor(sample, key)}");
             Console.ReadKey();
         }
 
+
         public static string CaesarCypherEncryptor(string str, int key)
         {
-            key = key % 26;
-            Dictionary<char, int> dictionary = new Dictionary<char, int>();
-            for (char c = 'a'; c <= 'z'; c++)
+            var numberOfChars = 26;
+            var resultBuilder = new StringBuilder();
+
+            for (int i = 0; i < str.Length; i++)
             {
-                int keyChar = c - 'a' + 1;
-                dictionary.Add(c, keyChar);
+                var encodedCharacter = (((str[i] - 97) + key) % numberOfChars) + 97;
+                resultBuilder.Append((char)encodedCharacter);
             }
-
-            string cypherString = "";
-            foreach (char character in str)
-            {
-                int index;
-                dictionary.TryGetValue(character, out index);
-
-                if (index + key > 26)
-                    index = (index + key) - 26;
-                else
-                    index = index + key;
-
-                char encryptedChar = dictionary.Where(a => a.Value == index).Select(a => a.Key).FirstOrDefault();
-                cypherString += encryptedChar;
-            }
-
-            return cypherString;
+            return resultBuilder.ToString();
         }
+
+        #region MySolution
+        //public static string CaesarCypherEncryptor(string str, int key)
+        //{
+        //    key = key % 26;
+        //    Dictionary<char, int> dictionary = new Dictionary<char, int>();
+        //    for (char c = 'a'; c <= 'z'; c++)
+        //    {
+        //        int keyChar = c - 'a' + 1;
+        //        dictionary.Add(c, keyChar);
+        //    }
+
+        //    string cypherString = "";
+        //    foreach (char character in str)
+        //    {
+        //        int index;
+        //        dictionary.TryGetValue(character, out index);
+
+        //        if (index + key > 26)
+        //            index = (index + key) - 26;
+        //        else
+        //            index = index + key;
+
+        //        char encryptedChar = dictionary.Where(a => a.Value == index).Select(a => a.Key).FirstOrDefault();
+        //        cypherString += encryptedChar;
+        //    }
+
+        //    return cypherString;
+        //}
+        #endregion
+
+
     }
 }
