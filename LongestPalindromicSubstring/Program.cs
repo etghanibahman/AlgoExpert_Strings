8d8efac8779c514e4c3b845905e8264e1fc7cd44
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-01-09 21:46:37 +0100

    Longest Palindromic Substring

--- LongestPalindromicSubstring/Program.cs
+++ LongestPalindromicSubstring/Program.cs
@@ -10,43 +10,84 @@ namespace LongestPalindromicSubstring
         {
             string str = "abaxyzzyxf";
             //str = "a";
-            str = "it's highnoon";
+            //str = "it's highnoon";
+            str = "non";
+            str = "noon";
 
-            Console.WriteLine($"The longest Palindrome is : {LongestPalindromicSubstring(str)}");
+            Console.WriteLine($"The longest Palindrome is : { LongestPalindromicSubstring(str) }");
         }
 
+        #region AlgoExpert_Solution
         public static string LongestPalindromicSubstring(string str)
         {
-            Dictionary<string, int> dicPalindromes = new Dictionary<string, int>();
+            string longestPalindrome = "";
             for (int i = 0; i < str.Length; i++)
             {
-                for (int j = i; j <= str.Length ; j++)
+                var palEven = ExpandFromMiddle(str, i, i + 1);
+                var palOdd = ExpandFromMiddle(str, i, i);
+
+                if (palOdd.Length > longestPalindrome.Length)
                 {
-                    string current = str.Substring(i, j - i);
-                    if (IsPalindrome(current))
-                    {
-                        if (!dicPalindromes.ContainsKey(current))
-                            dicPalindromes.Add(current, current.Length);
-                    }
+                    longestPalindrome = palOdd;
+                }
+                if (palEven.Length > longestPalindrome.Length)
+                {
+                    longestPalindrome = palEven;
                 }
             }
-            int maxLentgh = dicPalindromes?.Max(a=> a.Value) ?? 0;
-            return dicPalindromes.Where(a => a.Value == maxLentgh).FirstOrDefault().Key;
+            return longestPalindrome;
         }
 
-        public static bool IsPalindrome(string str)
+
+        public static string ExpandFromMiddle(string str, int i, int j)
         {
-            int leftIndex = 0;
-            int rightIndex = str.Length - 1;
-
-            while (leftIndex < rightIndex)
+            while (i >= 0 && j < str.Length && str[i] == str[j])
             {
-                if (str[leftIndex] != str[rightIndex])
-                    return false;
-                leftIndex += 1;
-                rightIndex -= 1;
+                i--;
+                j++;
             }
-            return true;
+            var currentLength = j - i - 1;
+            var startIndex = i + 1;
+            return str.Substring(startIndex, currentLength);
         }
+        #endregion
+
+
+        #region My_Solution
+        //public static string LongestPalindromicSubstring(string str)
+        //{
+        //    Dictionary<string, int> dicPalindromes = new Dictionary<string, int>();
+        //    for (int i = 0; i < str.Length; i++)
+        //    {
+        //        for (int j = i; j <= str.Length; j++)
+        //        {
+        //            string current = str.Substring(i, j - i);
+        //            if (IsPalindrome(current))
+        //            {
+        //                if (!dicPalindromes.ContainsKey(current))
+        //                    dicPalindromes.Add(current, current.Length);
+        //            }
+        //        }
+        //    }
+        //    int maxLentgh = dicPalindromes?.Max(a => a.Value) ?? 0;
+        //    return dicPalindromes.Where(a => a.Value == maxLentgh).FirstOrDefault().Key;
+        //}
+
+        //public static bool IsPalindrome(string str)
+        //{
+        //    int leftIndex = 0;
+        //    int rightIndex = str.Length - 1;
+
+        //    while (leftIndex < rightIndex)
+        //    {
+        //        if (str[leftIndex] != str[rightIndex])
+        //            return false;
+        //        leftIndex += 1;
+        //        rightIndex -= 1;
+        //    }
+        //    return true;
+        //}
+        #endregion
+
     }
 }
