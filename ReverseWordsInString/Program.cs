74c0e07b8629e6302183e313022a44b2f097f08d
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-03-25 23:59:49 +0100

    everse word in string

---
+++ ReverseWordsInString/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace ReverseWordsInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "AlgoExpert is the best!";
+
+            Console.WriteLine($"The string is: {str}");
+            Console.WriteLine($"The Reversed string is: { ReverseWordsInString(str)}");
+        }
+
+        public static string ReverseWordsInString(string str)
+        {
+            var builder = new StringBuilder();
+            int currentPosition = 0;
+
+            while (currentPosition < str.Length)
+            {
+                if (char.IsWhiteSpace(str[currentPosition]))
+                {
+                    builder.Insert(0, str[currentPosition]);
+                    currentPosition++;
+                    continue;
+                }
+
+                int wordStart = currentPosition;
+                while (currentPosition < str.Length && !char.IsWhiteSpace(str[currentPosition]))
+                {
+                    currentPosition++;
+                }
+
+                int wordLength = currentPosition - wordStart;
+                builder.Insert(0, str.Substring(wordStart, wordLength));
+            }
+
+            return builder.ToString();
+        }
+    }
+}
