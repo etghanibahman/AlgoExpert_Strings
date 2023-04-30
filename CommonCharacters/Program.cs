7c9e69514af9cab2aa8a6e8e8e037d863e2c3c55
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-04-30 15:17:02 +0200

    Common characters

---
+++ CommonCharacters/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CommonCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = new string[] { "abc", "bcd", "cbad" }; // {"b","c"}
+            Console.WriteLine($"Common characters are :{String.Join(',', CommonCharacters(strings))}");
+        }
+
+        //O(n*m) time, O(m) space
+        public static string[] CommonCharacters(string[] strings)
+        {
+            List<string> result = new List<string>();
+
+            var min = strings.Min(a => a.Length);
+            var toBeginWith = strings.FirstOrDefault(a => a.Length == min);
+            bool check = true;
+            foreach (char character in toBeginWith)
+            {
+                check = true;
+                foreach (var item in strings)
+                {
+                    if (!item.Contains(character)) {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check)
+                    result.Add(character.ToString());
+            }
+
+            return result.Distinct().ToArray();
+        }
+    }
+}
