b96a8cab2711a8549999350203df88845b3997d7
bahman etghani <etghanibahman@gmail.com>
2023-06-19 00:08:28 +0200

    initiating - OneEdit

---
+++ OneEdit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OneEdit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringOne = "hello";
+            string stringTwo = "hollo";
+            Console.WriteLine($"is one edit enough for <{stringOne}> and <{stringTwo}>? {OneEdit(stringOne,stringTwo)}");
+        }
+        
+        public static bool OneEdit(string stringOne, string stringTwo)
+        {
+            // Write your code here.
+            return false;
+        }
+    }
+}
