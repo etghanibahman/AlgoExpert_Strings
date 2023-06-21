74ffb86670ffe352c2411eceb398d6c2b8633632
bahman etghani <etghanibahman@gmail.com>
2023-06-21 22:58:03 +0200

    restrucring solution

---
+++ Hard/PatternMatcher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PatternMatcher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern= "xxyxxy";
+            string str = "gogopowerrangergogopowerranger";
+            Console.WriteLine($"Pattern: {pattern} , str: {str}");
+            Console.WriteLine($"The result is : {String.Join(',',PatternMatcher(pattern, str))}");
+        }
+
+        public static string[] PatternMatcher(string pattern, string str)
+        {
+            // Write your code here.
+            return new string[] { };
+        }
+    }
+}
